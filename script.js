aefef836eeafe38bbc0128aebe25f7f56f1d3f87
jaylearnjs <jaylearnjs@gmail.com>
2022-02-14 22:02:45 +0530

    End

--- script.js
+++ script.js
@@ -95,6 +95,7 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 // - Add ... between elements and start and end of string
 // - Log string to console
 
+/*
 const data1 = [17, 21, 23];
 const data2 = [12, 5, -5, 0, 4];
 
@@ -108,3 +109,4 @@ const printForecast = function (arr) {
   console.log('...' + str);
 };
 printForecast(data1);
+*/
